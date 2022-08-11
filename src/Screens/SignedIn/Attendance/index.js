46e25754718a97fc146cba2fcbc02e1d498ca3ae
SevkiUzun <sevki.meu@gmail.com>
2022-08-11 12:08:05 +0300

    AkridaDemoV4
    AttendanceScreen,FoodScreen, CalendarScreen sayfaları düzenlendi.

--- src/Screens/SignedIn/Attendance/index.js
+++ src/Screens/SignedIn/Attendance/index.js
@@ -1,6 +1,7 @@
 import {StyleSheet, Text, View, FlatList, Image, Animated} from 'react-native';
 import React, {useState, useEffect, useRef} from 'react';
 import Feather from 'react-native-vector-icons/Feather';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 const Attendance = () => {
   function PressYes() {
     console.warn('Yes');
@@ -19,44 +20,101 @@ const Attendance = () => {
   useEffect(() => {
     getApi();
   }, []);
-  return (
-    <View style={{flex: 1, backgroundColor: '#fff'}}>
+
+  const renderItem = ({item, index}) => {
+    const onClickItem = deger => {
+      const newArrData = data.map(e => {
+        // newArrData = [...newArrData]
+        if (item.id === e.id) {
+          if (deger === 'button1') {
+            return {
+              ...e,
+              selected: true,
+              deger: 'button1',
+            };
+          } else if (deger === 'button2') {
+            return {
+              ...e,
+              selected: true,
+              deger: 'button2',
+            };
+          }
+        } else {
+          return {
+            ...e,
+          };
+        }
+      });
+      setData(newArrData);
+    };
+    const ButtonRender = () => {
+      return (
+        <>
+          <TouchableOpacity
+            style={{
+              backgroundColor:
+                item.selected && item.deger === 'button1' ? 'green' : '#d3d3d3',
+              borderRadius: 20,
+              marginStart: 20,
+            }}
+            onPress={() => onClickItem('button1')}>
+            <Feather name="check" size={30} />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              left:10,
+              backgroundColor:
+                item.selected && item.deger === 'button2' ? 'red' : '#d3d3d3',
+              borderRadius: 20,
+            }}
+            onPress={() => onClickItem('button2')}>
+            <Feather name="x" size={30} />
+          </TouchableOpacity>
+        </>
+      );
+    };
+    return (
       <View
         style={{
-          width:'90%',
           flexDirection: 'row',
           padding: 20,
-          margin: 20,
+          marginBottom: 20,
           backgroundColor: 'rgba(255,255,255,0.9)',
           borderRadius: 22,
           shadowColor: '#000',
           shadowOffset: {
             width: 0,
-            height: 5,
+            height: 10,
           },
           shadowOpacity: 0.3,
           shadowRadius: 20,
           elevation: 3,
         }}>
+        <Image
+          source={{uri: item.thumbnail}}
+          style={{width: 70, height: 70, borderRadius: 70, marginRight: 10}}
+        />
         <View>
-          <Text style={{margin:10,fontSize: 22, fontWeight: '700'}}>Tümünü Seç</Text>
+          <Text style={{fontSize: 22, fontWeight: '700'}}>{item.name}</Text>
+          <Text style={{fontSize: 22, fontWeight: '700'}}>{item.lastname}</Text>
+          <Text style={{fontSize: 18, opacity: 0.7, color: 'blue'}}>
+            {item.school}
+          </Text>
         </View>
         <View
           style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            paddingLeft: 77,
             flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}>
-          <Feather
-            name="check"
-            size={30}
-            style={{padding: 10}}
-            onPress={PressYes}
-          />
-          <Feather name="x" size={30} onPress={PressNo} />
+          <ButtonRender />
         </View>
       </View>
+    );
+  };
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
       <Animated.FlatList
         data={data}
         onScroll={Animated.event(
@@ -65,51 +123,7 @@ const Attendance = () => {
         )}
         keyExtractor={item => item.id}
         contentContainerStyle={{padding: 20}}
-        renderItem={({item, index}) => (
-          <View
-            style={{
-              flexDirection: 'row',
-              padding: 20,
-              marginBottom: 20,
-              backgroundColor: 'rgba(255,255,255,0.9)',
-              borderRadius: 22,
-              shadowColor: '#000',
-              shadowOffset: {
-                width: 0,
-                height: 10,
-              },
-              shadowOpacity: 0.3,
-              shadowRadius: 20,
-              elevation: 3,
-            }}>
-            <Image
-              source={{uri: item.thumbnail}}
-              style={{width: 70, height: 70, borderRadius: 70, marginRight: 10}}
-            />
-            <View>
-              <Text style={{fontSize: 22, fontWeight: '700'}}>{item.name}</Text>
-              <Text style={{fontSize: 22, fontWeight: '700'}}>{item.lastname}</Text>
-              <Text style={{fontSize: 18, opacity: 0.7, color: 'blue'}}>
-                {item.school}
-              </Text>
-            </View>
-            <View
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                marginStart: 30,
-                flexDirection: 'row',
-              }}>
-              <Feather
-                name="check"
-                size={30}
-                style={{padding: 10}}
-                onPress={PressYes}
-              />
-              <Feather name="x" size={30} onPress={PressNo} />
-            </View>
-          </View>
-        )}
+        renderItem={renderItem}
       />
     </View>
   );
--- src/Screens/SignedIn/CalendarScreen/index.js
+++ src/Screens/SignedIn/CalendarScreen/index.js
@@ -1,69 +1,65 @@
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import React, {useState} from 'react';
+import {addDays, format} from 'date-fns';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {Agenda} from 'react-native-calendars';
-import {Card, Avatar} from 'react-native-paper';
 
-const timeToString = time => {
-  const date = new Date(time);
-  return date.toISOString().split('T')[0];
-};
+
+const data = {
+  '2022-08-11' : [
+    {
+      name: 'test 1' , status : 'Var'
+    },
+    {
+      name: 'test 2' , status : 'Var'
+    },
+    {
+      name: 'test 3' , status : 'yok'
+    },
+    {
+      name: 'test 4' , status : 'yok'
+    },
+  ],
+  '2022-08-12':[
+    {
+      name: 'test 5' , status : 'yok'
+    },
+    {
+      name: 'test 6' , status : 'var'
+    },
+  ]
+}
 
 const CalendarScreen = () => {
-  const [items, setItems] = useState({});
-  const loadItems = day => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
-              height: Math.max(50, Math.floor(Math.random() * 150)),
-            });
-          }
-        }
-      }
-      const newItems = {};
-      Object.keys(items).forEach(key => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
-  };
-  const renderItem = item => {
+  const [items, setItems] = useState(data)
+
+  const renderItem = (item) => {
     return (
-      <TouchableOpacity style={{marginRight: 10, marginTop: 17}}>
-        <Card>
-          <Card.Content>
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              }}>
-              <Text>{item.name}</Text>
-              <Avatar.Text label="J" />
-            </View>
-          </Card.Content>
-        </Card>
-      </TouchableOpacity>
+      <View style={styles.itemContainer}>
+        <Text>{item.name}</Text>
+        <Text>{item.status}</Text>
+      </View>
     );
   };
+
   return (
-    <View style={{flex: 1}}>
-      <Agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={'2023-08-07'}
-        renderItem={renderItem}
-      />
-    </View>
+    <SafeAreaView style={styles.safe}>
+      <Agenda items={items} renderItem={renderItem} />
+    </SafeAreaView>
   );
 };
 
-export default CalendarScreen;
+const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+  },
+  itemContainer: {
+    backgroundColor: 'white',
+    margin: 5,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default CalendarScreen;
