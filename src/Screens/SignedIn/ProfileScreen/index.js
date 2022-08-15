3b7570ab34bb8cfafa969ef8a705bc4e31a2f3bd
Nurullah Tuğlu <nurullah.tuglu@gmail.com>
2022-08-15 17:50:12 +0300

    ProfileScreen is modified

--- src/Screens/SignedIn/ProfileScreen/index.js
+++ src/Screens/SignedIn/ProfileScreen/index.js
@@ -1,60 +1,120 @@
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import React, {useState, useEffect} from 'react';
-import {Avatar, Title, Subheading, Button} from 'react-native-paper';
-import auth from '@react-native-firebase/auth';
-const ProfileScreen = ({navigation, route}) => {
-  const [name, SetName] = useState('');
-  const [email, SetEmail] = useState('');
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-  const signOut = () => {
-    auth().signOut();
-  };
 
-  useEffect(() => {
-    auth().onAuthStateChanged(user => {
-      SetName(user?.displayName ?? 'Boş');
-      SetEmail(user?.email ?? '');
-    });
-  }, []);
+const ProfileScreen = () => {
+  const [ogrAd,setOgrAd] = useState("Ahmet Öğretmen");
+  const [ogrMail,setOgrMail] = useState("ogr@hotmail.com");
+  const [ogrTelefon,setOgrTelefon] = useState("02124190505");
+  const [ogrSifre,setOgrSifre] = useState("123456");
+
+  const [okulAd,setOkulAd] = useState("Akrida Çamlıca");
+  const [okulTelefon,setOkulTelefon] = useState("02163262448");
+  const [okulAdres,setOkulAdres] = useState("Bulgurlu, Blugurlu Cd. No:12, 34760 Üsküdar/İstanbul");
+  const [okulYetkili,setOkulYetkili] = useState("Büşra Aklan");
+  const [okulYetkiliTelefon,setOkulYetkiliTelefon] = useState("05352657888");
+
+
+  function saveChanges(){
+    alert("Değişiklikleri Kaydet");
+  }
+
+  function signOut(){
+    alert("Çıkış Yap");
+  }
   return (
-    <View style={styles.mainView}>
-      <Avatar.Text
-        label={name
-          .split(' ')
-          .reduce((prev, current) => prev + current[0] + current[1], '')}
-      />
-      <Title>{name}</Title>
-      <Subheading>{email}</Subheading>
-      <TouchableOpacity onPress={signOut} style={styles.Button}>
-        <Text style={styles.ButtonText}>Sign out</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "whitesmoke", height: "100%" }}>
+      
+      <View style={{ justifyContent: "center", alignItems: "center", backgroundColor: "white", height: 180, borderBottomWidth: 0.5 }}>
+        <View style={{ backgroundColor: "#93CAED", borderRadius: 50, height: 110, width: 110, justifyContent: "center", alignItems: "center" }}>
+          <Icon name="user" size={74} />
+        </View>
+        <Text style={{ fontSize: 22 }}>{ogrAd}</Text>
+      </View>
+      <ScrollView>
+        <View style={{ borderWidth:0.5,flexDirection: "row", backgroundColor: "whitesmoke", height: 60 }}>
+          <TouchableOpacity style={{ flexDirection:"row",justifyContent:"center",alignItems:"center", flex: 1 }}>
+            <Icon name='user' size={45}/>
+            <Text style={{fontSize:25,paddingLeft:5,fontStyle:"italic",fontWeight:"bold"}}>Öğretmen Bilgileri</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Adı</Text>
+          <TextInput style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15}}>{ogrAd}</Text>
+          </TextInput>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Mail</Text>
+          <TextInput style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15}}>{ogrMail}</Text>
+          </TextInput>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Telefon</Text>
+          <TextInput style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15}}>{ogrTelefon}</Text>
+          </TextInput>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Şifre</Text>
+          <TextInput style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15}}>{ogrSifre}</Text>
+          </TextInput>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <TouchableOpacity onPress={()=>saveChanges()} style={{backgroundColor:"#93CAED",height:35,justifyContent:"center",alignItems:"center"}}><Text>Kaydet</Text></TouchableOpacity>
+        </View>
+        
+        <View style={{ borderWidth:0.5,flexDirection: "row", backgroundColor: "whitesmoke", height: 60 }}>
+          <TouchableOpacity style={{ flexDirection:"row",justifyContent:"center",alignItems:"center", flex: 1 }}>
+            <Icon name='building' size={45}/>
+            <Text style={{fontSize:25,paddingLeft:5,fontStyle:"italic",fontWeight:"bold"}}>Okul Bilgileri</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Adı</Text>
+          <TextInput editable={false} style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15,fontWeight:"bold"}}>{okulAd}</Text>
+          </TextInput>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Telefon</Text>
+          <TextInput editable={false} style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15,fontWeight:"bold"}}>{okulTelefon}</Text>
+          </TextInput>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Adres</Text>
+          <TextInput editable={false} style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15,fontWeight:"bold"}}>{okulAdres}</Text>
+          </TextInput>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Yetkili</Text>
+          <TextInput editable={false} style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15,fontWeight:"bold"}}>{okulYetkili}</Text>
+          </TextInput>
+        </View>
+        <View style={{borderBottomWidth:0.5}}>
+          <Text style={{fontSize:15,color:"black",paddingStart:15,paddingTop:5}}>Yetkili Telefon</Text>
+          <TextInput editable={false} style={{paddingLeft:15,paddingBottom:0,paddingTop:0}}>
+            <Text style={{fontSize:15,fontWeight:"bold"}}>{okulYetkiliTelefon}</Text>
+          </TextInput>
+        </View>
+        <View style={{ flexDirection: "row", justifyContent: "flex-end", height: 40 }}>
+        <TouchableOpacity onPress={()=>signOut()} style={{ flexDirection: "row", height: "100%", alignItems: "center" }}>
+          <Text style={{ marginRight: 8, fontWeight: "bold", fontSize: 17,color:"#FF3333" }}>Çıkış Yap</Text>
+          <Icon name="arrow-right" size={30} style={{color:"#FF3333"}}/>
+        </TouchableOpacity>
+      </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default ProfileScreen;
 
-const styles = StyleSheet.create({
-  mainView: {
-    marginTop: 40,
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  Button: {
-    width: '90%',
-    color: '#000',
-    height: 52,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    marginTop: 20,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  ButtonText: {
-    fontWeight: 'bold',
-    fontSize: 18,
-  },
-});
+const styles = StyleSheet.create({});
